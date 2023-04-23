5c3d7d634f37c720edc9fb1cc729105d46bb1dea
mike-pc <tn.mike@hotmail.com>
2023-04-23 16:17:29 +0700

    [ADD] : DbContext

---
+++ DemoDotNetAPI/CustomerAPI/CustomerAPI/DataAccess/CustomerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAPI.DataAccess
+{
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerInfoEntity>? custInfoEntity { get; set; }
+    }
+}
